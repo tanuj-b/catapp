2c6c66e92666fd87f16cbe1a4678e080007a5c35
ssachan <shikhar.sachan@gmail.com>
2012-08-27 14:16:09 +0530

    modularizing

--- js/main.js
+++ js/main.js
@@ -40,30 +40,3 @@ Backbone.View.prototype.close = function(){
   }
 };
 
-$(document).ready(function() {
-
-	utils.loadTemplate([ 'LandingView', 'QuizQuestionView','FlashCardListView', 'FlashCardListItemView', 'FlashCardView', 'MenuView','PracticeTopicsView','PracticeQuestionView','QuizTopicsView','QuizResultsView','ProfileView','QuizAnalyticsView' ], function() {
-		app = new AppRouter();
-		(function(d){
-		      var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-		      js = d.createElement('script'); js.id = id; js.async = true;
-		      js.src = "//connect.facebook.net/en_US/all.js";
-		      d.getElementsByTagName('head')[0].appendChild(js);
-		}(document));
-		Backbone.history.start();
-	});
-});
-
-var onDeviceReady = function(){
-	console.log('on device ready');
-	utils.loadTemplate([ 'LandingView', 'QuizQuestionView','FlashCardListView', 'FlashCardListItemView', 'FlashCardView', 'MenuView','PracticeTopicsView','PracticeQuestionView','QuizTopicsView','QuizResultsView','ProfileView','QuizAnalyticsView'  ], function() {
-		app = new AppRouter();
-		(function(d){
-		      var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-		      js = d.createElement('script'); js.id = id; js.async = true;
-		      js.src = "//connect.facebook.net/en_US/all.js";
-		      d.getElementsByTagName('head')[0].appendChild(js);
-		}(document));
-		Backbone.history.start();
-	});	
-};
