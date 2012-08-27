ff562fae89279c4b5d395d716f83d6ca25241450
ssachan <shikhar.sachan@gmail.com>
2012-08-27 15:28:04 +0530

    latest working

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ var currentQuiz = null;
 
 var activeFlashCardView = new FlashCardView();
 
-var AppView = function AppView(){
+var AppView = function AppView() {
 
-  /* this.showView(view) = function(){
+    /* this.showView(view) = function(){
     if (this.currentView){
       this.currentView.close();
     }
@@ -29,14 +29,56 @@ var AppView = function AppView(){
 
 };
 
-Backbone.View.prototype.close = function(){
-  if (this.beforeClose){
-    this.beforeClose();
-  };
-  this.remove();
-  this.unbind();
-  if (this.onClose){
-    this.onClose();
-  }
+Backbone.View.prototype.close = function () {
+    if (this.beforeClose) {
+        this.beforeClose();
+    };
+    this.remove();
+    this.unbind();
+    if (this.onClose) {
+        this.onClose();
+    }
 };
 
+if (phonegap == false) {
+    $(document).ready(function () {
+        helper.loadTemplate(['LandingView', 'QuizQuestionView', 'FlashCardListView', 'FlashCardListItemView', 'FlashCardView', 'MenuView', 'PracticeTopicsView', 'PracticeQuestionView', 'QuizTopicsView', 'QuizResultsView', 'ProfileView', 'QuizAnalyticsView'], function () {
+            app = new AppRouter();
+            (function (d) {
+                var js, id = 'facebook-jssdk';
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement('script');
+                js.id = id;
+                js.async = true;
+                js.src = "//connect.facebook.net/en_US/all.js";
+                d.getElementsByTagName('head')[0].appendChild(js);
+            }(document));
+            Backbone.history.start();
+        });
+    });
+}
+
+function init() {
+    document.addEventListener("deviceready", onDeviceReady, true);
+}
+
+var onDeviceReady = function () {
+    console.log('on device ready');
+    helper.loadTemplate(['LandingView', 'QuizQuestionView', 'FlashCardListView', 'FlashCardListItemView', 'FlashCardView', 'MenuView', 'PracticeTopicsView', 'PracticeQuestionView', 'QuizTopicsView', 'QuizResultsView', 'ProfileView', 'QuizAnalyticsView'], function () {
+        app = new AppRouter();
+        (function (d) {
+            var js, id = 'facebook-jssdk';
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement('script');
+            js.id = id;
+            js.async = true;
+            js.src = "//connect.facebook.net/en_US/all.js";
+            d.getElementsByTagName('head')[0].appendChild(js);
+        }(document));
+        Backbone.history.start();
+    });
+};
