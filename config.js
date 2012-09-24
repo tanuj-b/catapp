98dcfbabc67f1ae9f706b38000200c736905b8bd
ssachan <shikhar.sachan@gmail.com>
2012-09-24 18:43:31 +0005

    modified config to include server configuration details

--- config.js
+++ config.js
@@ -51,34 +51,59 @@ var config_mobile_web = {
 };
 
 
-var config_mobile_testrex = {
-	syncStoragePath : "",
-	templatePath : "",
-	serverUrl : 'http://www.test-rex.com/tanuj/api/',
-	edition : '0',
-    tmplatesFolder : "tpl-mobile",
-    phonegap : false,
-    viewsArray : [	'LandingView', 
-    				'QuizQuestionView',
-    				'FlashCardListView',
-    				'FlashCardListItemView',
-    				'FlashCardView',
-    				'MenuView',
-    				'PracticeTopicsView',
-    				'PracticeQuestionView',
-    				'QuizTopicsView',
-    				'QuizResultsView',
-    				'ProfileView',
-    			]
-};
+var config_mobile_server = {
+		syncStoragePath : "",
+		templatePath : "",
+		serverUrl : 'http://www.test-rex.com/prod/api/',
+		edition : '0',
+	    tmplatesFolder : "tpl-mobile",
+	    phonegap : false,
+	    viewsArray : [	'LandingView', 
+	    				'QuizQuestionView',
+	    				'FlashCardListView',
+	    				'FlashCardListItemView',
+	    				'FlashCardView',
+	    				'MenuView',
+	    				'PracticeTopicsView',
+	    				'PracticeQuestionView',
+	    				'QuizTopicsView',
+	    				'QuizResultsView',
+	    				'ProfileView',
+	    			]
+	};
 
-//var Config = config_mobile_android;
+var config_web_server = {
+		syncStoragePath : "",
+		templatePath : "",
+		serverUrl : 'http://www.test-rex.com/prod/api/',
+		edition : '1',
+	    tmplatesFolder : "tpl-web",
+	    phonegap : false,
+	    viewsArray : [	'HeaderView',
+	                  	'LandingView',
+	                  	'MenuView',
+	                  	'QuizView',
+	    				'QuizQuestionView',
+	    				'FlashCardListView',
+	    				'FlashCardListItemView',
+	    				'FlashCardView',
+	    				'MenuView',
+	    				'PracticeTopicsView',
+	    				'PracticeView',
+	    				'PracticeQuestionView',
+	    				'QuizTopicsView',
+	    				'QuizResultsView',
+	    				'ProfileView',
+	    				'PerformanceView',
+	    				'QuizAnalyticsView'
+	    			]
+	};
+
 var Config = null;
 if($(window).width()<700){
-	Config = config_mobile_android;
+	Config = config_mobile_server;
 }else {
-	Config = config_mobile_web;
+	Config = config_web_server;
 }
-//var Config = config_mobile_web;	
     
 
