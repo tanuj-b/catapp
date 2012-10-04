c78d0426835175969bef39c4fcf16301a4a85b74
ssachan <shikhar.sachan@gmail.com>
2012-10-05 03:26:31 +0530

    minor changes on the dashboard

--- js/utils/sync.js
+++ js/utils/sync.js
@@ -1,106 +1,23 @@
 window.dao =  {
 
-    syncURL: "../api/employees",
+    syncQuizzes: "../api/quizzes",
 
     initialize: function(callback) {
-        var self = this;
-        this.db = window.openDatabase("syncdemodb", "1.0", "Sync Demo DB", 200000);
-
-        // Testing if the table exists is not needed and is here for logging purpose only. We can invoke createTable
-        // no matter what. The 'IF NOT EXISTS' clause will make sure the CREATE statement is issued only if the table
-        // does not already exist.
-        this.db.transaction(
-            function(tx) {
-                tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='employee'", this.txErrorHandler,
-                    function(tx, results) {
-                        if (results.rows.length == 1) {
-                            log('Using existing Employee table in local SQLite database');
-                        }
-                        else
-                        {
-                            log('Employee table does not exist in local SQLite database');
-                            self.createTable(callback);
-                        }
-                    });
-            }
-        );
-
     },
         
-    createTable: function(callback) {
-        this.db.transaction(
-            function(tx) {
-                var sql =
-                    "CREATE TABLE IF NOT EXISTS employee ( " +
-                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    "firstName VARCHAR(50), " +
-                    "lastName VARCHAR(50), " +
-                    "title VARCHAR(50), " +
-                    "officePhone VARCHAR(50), " +
-                    "deleted INTEGER, " +
-                    "lastModified VARCHAR(50))";
-                tx.executeSql(sql);
-            },
-            this.txErrorHandler,
-            function() {
-                log('Table employee successfully CREATED in local SQLite database');
-                callback();
-            }
-        );
-    },
-
-    dropTable: function(callback) {
-        this.db.transaction(
-            function(tx) {
-                tx.executeSql('DROP TABLE IF EXISTS employee');
-            },
-            this.txErrorHandler,
-            function() {
-                log('Table employee successfully DROPPED in local SQLite database');
-                callback();
-            }
-        );
-    },
-
-    findAll: function(callback) {
-        this.db.transaction(
-            function(tx) {
-                var sql = "SELECT * FROM EMPLOYEE";
-                log('Local SQLite database: "SELECT * FROM EMPLOYEE"');
-                tx.executeSql(sql, this.txErrorHandler,
-                    function(tx, results) {
-                        var len = results.rows.length,
-                            employees = [],
-                            i = 0;
-                        for (; i < len; i = i + 1) {
-                            employees[i] = results.rows.item(i);
-                        }
-                        log(len + ' rows found');
-                        callback(employees);
-                    }
-                );
-            }
-        );
-    },
-
     getLastSync: function(callback) {
-        this.db.transaction(
-            function(tx) {
-                var sql = "SELECT MAX(lastModified) as lastSync FROM employee";
-                tx.executeSql(sql, this.txErrorHandler,
-                    function(tx, results) {
-                        var lastSync = results.rows.item(0).lastSync;
-                        log('Last local timestamp is ' + lastSync);
-                        callback(lastSync);
-                    }
-                );
+    	var url = Config.serverUrl+'getLastSync/';
+        $.ajax({
+            url:url,
+            dataType:"json",
+            success:function (data) {
+                console.log("search success: " + data.length);
             }
-        );
+        });
     },
 
     sync: function(callback) {
-
-        var self = this;
+    	var self = this;
         log('Starting synchronization...');
         this.getLastSync(function(lastSync){
             self.getChanges(self.syncURL, lastSync,
@@ -139,7 +56,7 @@ window.dao =  {
             function(tx) {
                 var l = employees.length;
                 var sql =
-                    "INSERT OR REPLACE INTO employee (id, firstName, lastName, title, officePhone, deleted, lastModified) " +
+                    "UPDATE quizzes (id) " +
                     "VALUES (?, ?, ?, ?, ?, ?, ?)";
                 log('Inserting or Updating in local database:');
                 var e;
@@ -162,48 +79,6 @@ window.dao =  {
         alert(tx.message);
     }
 };
-
-dao.initialize(function() {
-    console.log('database initialized');
-});
-
-$('#reset').on('click', function() {
-    dao.dropTable(function() {
-       dao.createTable();
-    });
-});
-
-
-$('#sync').on('click', function() {
-    dao.sync(renderList);
-});
-
-$('#render').on('click', function() {
-    renderList();
-});
-
-$('#clearLog').on('click', function() {
-    $('#log').val('');
-});
-
-function renderList(employees) {
-    log('Rendering list using local SQLite data...');
-    dao.findAll(function(employees) {
-        $('#list').empty();
-        var l = employees.length;
-        for (var i = 0; i < l; i++) {
-            var employee = employees[i];
-            $('#list').append('<tr>' +
-                '<td>' + employee.id + '</td>' +
-                '<td>' +employee.firstName + '</td>' +
-                '<td>' + employee.lastName + '</td>' +
-                '<td>' + employee.title + '</td>' +
-                '<td>' + employee.officePhone + '</td>' +
-                '<td>' + employee.deleted + '</td>' +
-                '<td>' + employee.lastModified + '</td></tr>');
-        }
-    });
-}
 
 function log(msg) {
     $('#log').val($('#log').val() + msg + '\n');
--- js/views-web/headerview.js
+++ js/views-web/headerview.js
@@ -3,6 +3,29 @@ window.HeaderView = Backbone.View.extend({
 	initialize : function() {
 		this.render();
 	},
+	
+	events : {
+		'click #sync' : 'onSyncClick',
+	},
+    
+	onSyncClick : function() {
+		// the list of completed unsynced quizzes
+    	var completedQuizzes = quizzes.where({hasAttempted:true,synced:false});
+    	var len = completedQuizzes.length;
+    	for (var i = 0; i < len ;i++){
+    		var qIds = completedQuizzes[i].getQuestionIds();
+    		var qlen = qIds.length;
+    		for(var j=0; j< qlen; j++){
+    			var question = quizQuestions.get(qIds[j]); 
+        		var response = new Response({accountId:'1', quizId:completedQuizzes[i].get('id'), questionId:qIds[j], optionSelected:question.get('optionSelected'), timeTaken:question.get('timeTaken')});
+        		helper.changeSync(1);
+        		response.save();
+    		}
+    		completedQuizzes[i].set('synced',true);
+    		helper.changeSync(2);
+    		completedQuizzes[i].save();
+    	}
+	},
 
 	menuChange : function(e) {
 		this.currentSelection = $(e.currentTarget).attr('id');
